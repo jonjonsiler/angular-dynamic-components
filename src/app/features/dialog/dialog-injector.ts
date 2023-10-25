4428c14916b72f1aa5ecc45b263f322452308ebf
Jonathan Siler <jonjonsiler@hotmail.com>
2023-10-24 23:45:36 -0500

    Add arg clarity to DialogInjector

--- src/app/features/dialog/dialog-injector.ts
+++ src/app/features/dialog/dialog-injector.ts
@@ -1,15 +1,27 @@
-import { Injector, Type, InjectionToken, InjectFlags } from '@angular/core';
+import { Injector, Type, InjectionToken, InjectFlags, InjectOptions } from '@angular/core';
 
 export class DialogInjector implements Injector {
-  constructor(private _parentInjector: Injector, private _additionalTokens: WeakMap<any, any>) {}
+  constructor(
+    private _parentInjector: Injector, 
+    private _additionalTokens: WeakMap<any, any>
+  ) {}
 
-  get<T>(token: Type<T> | InjectionToken<T>, notFoundValue?: T, flags?: InjectFlags): T;
-  get(token: any, notFoundValue?: any);
-  get(token: any, notFoundValue?: any, flags?: any) {
+  get<T>(
+    token: Type<T> | InjectionToken<T>, 
+    notFoundValue?: T,
+    options?: InjectOptions
+  ): T;
+  get(
+    token: any,
+    notFoundValue?: any
+  );
+  get(
+    token: any, 
+    notFoundValue?: any,
+    flags?: any
+  ) {
     const value = this._additionalTokens.get(token);
-
     if (value) return value;
-
     return this._parentInjector.get<any>(token, notFoundValue);
   }
 }
