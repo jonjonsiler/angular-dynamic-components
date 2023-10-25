2d5641a6f680b4e567461268f91b33bbd0cbd89f
Jonathan Siler <jonjonsiler@hotmail.com>
2023-10-24 23:44:57 -0500

    Update app component with inject fn for DI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { DialogService } from './features/dialog/dialog.service';
 import { ExampleComponent } from './features/example/example.component';
 
@@ -8,9 +8,9 @@ import { ExampleComponent } from './features/example/example.component';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor(public dialog: DialogService) {
+  public dialog: DialogService = inject(DialogService);
+  constructor() {
     const ref = this.dialog.open(ExampleComponent, { data: { message: 'I am a dynamic component inside of a dialog!' } });
-
     ref.afterClosed.subscribe(result => {
       console.log('Dialog closed', result);
     });
